60c6b4ca98024340b05fcb12791fd3dc214b0118
Juan Martin Villanueva <>
2023-11-02 22:00:53 -0300

    funcion settimeout con funcion anonima y no

--- src/callback/index.js
+++ src/callback/index.js
@@ -7,3 +7,14 @@ function calc(num1, num2, callback){
 }
 
 console.log(calc(2, 2, sum))
+
+setTimeout(function(){
+    console.log('Hola JavaScript');
+},2000);
+
+function grettin(name){
+    console.log(`Hola ${name}`);
+}
+
+setTimeout(grettin, 3000,'Juanma');
+
